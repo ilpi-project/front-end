ded3d9a67cd93c99a3c012493f57759ad55bbd6a
marcellacarneiro <carneiromarcella53@gmail.com>
2025-03-20 20:22:13 -0300

    ajusta index.tsx

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -3,7 +3,6 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
       <Stack.Screen name="Onboarding/index" />
       <Stack.Screen name="Login/index" />
       <Stack.Screen name="Home/index" />
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { Redirect } from "expo-router";
+import Onboarding from "@/app/Onboarding";
 
 export default function Index() {
-  return <Redirect href="/Onboarding" />;
+  return <Onboarding />
 }
