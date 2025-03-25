4ff8549eac3c7772fb86c3fcb9c47364ba391593
marcellacarneiro <carneiromarcella53@gmail.com>
2025-03-25 18:23:14 -0300

    estiliza tela de Login

--- src/app/Login/index.tsx
+++ src/app/Login/index.tsx
@@ -1,13 +1,29 @@
-import { View, Text, Button } from "react-native";
-import { useRouter } from "expo-router";
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import styles from './styles';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
 export default function Login() {
-  const router = useRouter();
-
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Login</Text>
-      <Button title="Ir para Home" onPress={() => router.push("/Home")} />
-    </View>
-  );
+    const router = useRouter();
+    const handleLogin = () => {
+        router.push('/Home'); // router.replace('/Home');
+    };
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image source={require('../assets/images/logo.png')} style={styles.logo} />
+            <View style={styles.inputsContainer}>
+                <Input icon="mail" placeholder="E-mail" keyboardType="email-address" />
+                <Input icon="lock-closed" placeholder="Senha" secureTextEntry />
+                <TouchableOpacity style={styles.forgotPasswordButton}>
+                    <Text style={styles.forgotPasswordText}>Esqueceu a senha?</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.buttonsContainer}>
+                <Button text="Login" variant="primary" style={{ width: '50%' }} onPress={handleLogin} />
+                <Button text="Cadastre-se" variant="secondary" style={{ width: '50%' }} />
+            </View>
+        </SafeAreaView>
+    );
 }
--- src/app/Login/styles.ts
+++ src/app/Login/styles.ts
@@ -1,5 +1,39 @@
 import { StyleSheet } from 'react-native';
+import colors from '../config/colors';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.beige,
+    },
+    logo: {
+        height: 200,
+        width: 200,
+        marginBottom: 20
+    },
+    inputsContainer: {
+        width: '75%',
+    },
+    forgotPasswordButton: {
+        backgroundColor: 'transparent',
+        marginTop: 12,
+    },
+    forgotPasswordText: {
+        color: colors.black,
+        textAlign: 'right',
+    },
+    buttonsContainer: {
+        width: '100%',
+        paddingHorizontal: 36,
+        paddingVertical: 40,
+        alignItems: 'center',
+        flexDirection: 'column',
+        position: 'relative',
+        top: 50,
+        gap: 16
+    },
+});
 
 export default styles;
