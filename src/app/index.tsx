a88ab7532b3d38bef8c66003765acbf994f6f93a
marcellacarneiro <carneiromarcella53@gmail.com>
2025-03-24 21:47:15 -0300

    estiliza carregamento do app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,49 @@
-import { Redirect } from "expo-router";
-import Onboarding from "@/app/Onboarding";
+import Onboarding from '@/app/Onboarding';
+import { View, Text, SafeAreaView, ActivityIndicator } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
 
 export default function Index() {
-  return <Onboarding />
+    const [appReady, setAppReady] = useState(false);
+
+    const [fontsLoaded] = useFonts({
+        'Oxygen-Light': require('@/app/assets/fonts/Oxygen-Light.ttf'),
+        'Oxygen-Regular': require('@/app/assets/fonts/Oxygen-Regular.ttf'),
+        'Oxygen-Bold': require('@/app/assets/fonts/Oxygen-Bold.ttf')
+    });
+
+    useEffect(() => {
+        async function prepare() {
+            try {
+                await SplashScreen.preventAutoHideAsync();
+
+                if (fontsLoaded) {
+                    setAppReady(true);
+                    await SplashScreen.hideAsync();
+                }
+            } catch (error) {
+                console.warn(error);
+            }
+        }
+
+        prepare();
+    }, [fontsLoaded]);
+
+    if (!appReady) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" color="#6B9829" />
+                <Text>Carregando...</Text>
+            </View>
+        );
+    }
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar style="dark" />
+            <Onboarding />
+        </SafeAreaView>
+    );
 }
