1352fcc20b0f87fce00c255e08452771c2addd54
marcellacarneiro <carneiromarcella53@gmail.com>
2025-03-24 21:46:37 -0300

    finaliza estilização da tela de onboarding

--- src/app/Onboarding/index.tsx
+++ src/app/Onboarding/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, Text, Image, TouchableOpacity, SafeAreaView } from 'react-native';
 import Swiper from 'react-native-swiper';
 import { useRouter } from 'expo-router';
 
@@ -10,54 +10,99 @@ export default function Onboarding() {
     const handleSkip = () => {
         router.push('/Login'); // router.replace('Login');
     };
+
+    const swiperRef = useRef<Swiper | null>(null);
+
+    const goToPrevious = () => {
+        swiperRef.current?.scrollBy(-1, true);
+    };
+
+    const goToNext = () => {
+        swiperRef.current?.scrollBy(1, true);
+    };
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
     return (
-        <Swiper
-            loop={false}
-            showsPagination={true}
-            dotColor="#A9C087"
-            activeDotColor="#6B9829"
-            paginationStyle={{ bottom: 50 }}>
-            <View style={styles.container}>
-                <TouchableOpacity style={styles.skipButton} onPress={handleSkip}>
-                    <Text style={styles.skipButtonText}>Pular</Text>
-                </TouchableOpacity>
-                <View style={styles.infosContainer}>
-                    <Image source={require('../assets/images/onboarding-img-1.png')} style={styles.image} />
-                    <Text style={styles.text}>
-                        O ILPI App é uma plataforma digital desenvolvida para facilitar a gestão e o acompanhamento de
-                        idosos em instituições de longa permanência.
-                    </Text>
+        <SafeAreaView style={styles.container}>
+            <Swiper
+                ref={swiperRef}
+                loop={false}
+                showsPagination={true}
+                dotColor="#A9C087"
+                activeDotColor="#6B9829"
+                activeDotStyle={{ width: 20 }}
+                paginationStyle={{ bottom: 30 }}
+                onIndexChanged={(index) => setCurrentIndex(index)}>
+                <View style={styles.slideContainer}>
+                    <View style={styles.slideHeader}>
+                        <TouchableOpacity style={styles.skipButton}>
+                            <Text style={styles.skipButtonText}>Pular</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.infosContainer}>
+                        <Image source={require('../assets/images/onboarding-img-1.png')} style={styles.image1} />
+                        <Text style={styles.title}>Bem-vindo ao ILPI App</Text>
+                        <Text style={styles.text}>
+                            O ILPI App é uma plataforma digital desenvolvida para facilitar a gestão e o acompanhamento
+                            de idosos em instituições de longa permanência.
+                        </Text>
+                    </View>
                 </View>
-            </View>
-            <View style={styles.container}>
-                <TouchableOpacity style={styles.skipButton} onPress={handleSkip}>
-                    <Text style={styles.skipButtonText}>Pular</Text>
-                </TouchableOpacity>
-                <View style={styles.infosContainer}>
-                    <Image source={require('../assets/images/onboarding-img-2.png')} style={styles.image} />
-                    <Text style={styles.text}>
-                        O aplicativo oferece recursos para familiares, cuidadores e administradores, garantindo um
-                        cuidado mais humanizado e eficiente.
-                    </Text>
+                <View style={styles.slideContainer}>
+                    <View style={styles.slideHeader}>
+                        <TouchableOpacity style={styles.skipButton}>
+                            <Text style={styles.skipButtonText}>Pular</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.infosContainer}>
+                        <Image source={require('../assets/images/onboarding-img-2.png')} style={styles.image2} />
+                        <Text style={styles.title}>Apoio e bem-estar</Text>
+                        <Text style={styles.text}>
+                            O aplicativo oferece recursos para familiares, cuidadores e administradores, garantindo um
+                            cuidado mais humanizado e eficiente.
+                        </Text>
+                    </View>
                 </View>
-            </View>
-            <View style={styles.container}>
-                <View style={styles.infosContainer}>
-                    <Image source={require('../assets/images/logo.png')} style={styles.smallImage} />
-                    <Text style={styles.text}>
-                        Crie sua conta e tenha acesso a todas as informações sobre nosso instituto!
-                    </Text>
-                    <Text style={styles.text}>
-                        Conheça nossa história, nossos valores e os serviços que oferecemos.
-                    </Text>
-                    <Text style={styles.text}>
-                        Cadastre-se agora e descubra como podemos fazer a diferença para você e sua família!
-                    </Text>
+                <View style={styles.slideContainer}>
+                    <View style={styles.infosContainer}>
+                        <View style={styles.slideHeader}></View>
+                        <Image source={require('../assets/images/logo.png')} style={styles.logo} />
+                        <Text style={styles.title}>Junte-se a nós</Text>
+                        <Text style={styles.text}>
+                            Crie sua conta e tenha acesso a todas as informações sobre nosso instituto!{' '}
+                        </Text>
+                        <Text style={styles.text}>
+                            Conheça nossa história, nossos valores e os serviços que oferecemos.
+                        </Text>
+                        <Text style={styles.text}>
+                            Cadastre-se agora e descubra como podemos fazer a diferença para você e sua família!
+                        </Text>
+                    </View>
                 </View>
-                <TouchableOpacity style={styles.button} onPress={handleSkip}>
-                  <Text style={styles.buttonText}>Vamos lá!</Text>
-                </TouchableOpacity>
-            </View>
-        </Swiper>
+            </Swiper>
+            {currentIndex < 2 && (
+                <View style={styles.navButtonsContainer}>
+                    {currentIndex === 1 && (
+                        <TouchableOpacity onPress={goToPrevious} style={styles.backButton}>
+                            <Text style={styles.backButtonText}>Anterior</Text>
+                        </TouchableOpacity>
+                    )}
+
+                    {currentIndex < 2 && (
+                        <TouchableOpacity onPress={goToNext} style={styles.nextButton}>
+                            <Text style={styles.nextButtonText}>Próximo</Text>
+                        </TouchableOpacity>
+                    )}
+                </View>
+            )}
+            {currentIndex === 2 && (
+                <View style={styles.startButtonContainer}>
+                    <TouchableOpacity onPress={handleSkip} style={styles.startButton}>
+                        <Text style={styles.startButtonText}>Vamos lá!</Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+        </SafeAreaView>
     );
 }
--- src/app/Onboarding/styles.ts
+++ src/app/Onboarding/styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import FONT from '@/app/config/fonts';
 
 const styles = StyleSheet.create({
     container: {
@@ -7,49 +8,118 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#F5E6CC',
     },
+    slideContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    slideHeader: {
+        width: '100%',
+        height: 120,
+        alignItems: 'flex-end',
+        padding: 40,
+        boxSizing: 'border-box',
+        justifyContent: 'center',
+    },
+    skipButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 24,
+        backgroundColor: 'transparent',
+    },
+    skipButtonText: {
+        fontFamily: FONT.OXYGEN_BOLD,
+        color: '#333734',
+        letterSpacing: 1,
+        fontSize: 18,
+    },
     infosContainer: {
         justifyContent: 'center',
         alignItems: 'center',
         width: '80%',
     },
-    image: {
-        height: 350,
-        width: 320,
+    image1: {
+        height: 310,
+        width: 290,
         marginBottom: 20,
+        marginTop: 15,
     },
-    smallImage: {
-        height: 200,
-        width: 205,
+    image2: {
+        height: 300,
+        width: 290,
+        marginBottom: 20,
+        marginTop: 15,
+    },
+    logo: {
+        height: 230,
+        width: 230,
         marginBottom: 30,
     },
+    title: {
+        fontFamily: FONT.OXYGEN_BOLD,
+        fontSize: 24,
+        marginBottom: 12,
+    },
     text: {
-        fontSize: 18,
+        fontFamily: FONT.OXYGEN_REGULAR,
+        fontSize: 16,
         width: '100%',
         color: '#333',
         marginBottom: 5,
         letterSpacing: 0.65,
-        textAlign: 'left',
+        textAlign: 'center',
         lineHeight: 25,
     },
-    skipButton: {
-        position: 'absolute',
-        top: 50,
-        right: 50,
+    navButtonsContainer: {
+        width: '100%',
+        position: 'relative',
+        flexDirection: 'row',
+        bottom: 0,
+        paddingHorizontal: 36,
+        paddingVertical: 40,
+        justifyContent: 'space-between',
     },
-    skipButtonText: {
-        fontSize: 20,
-    },
-    button: {
+    nextButton: {
         backgroundColor: '#2E5E3D',
-        height: 52,
-        width: '45%',
+        width: '35%',
+        padding: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 24,
+    },
+    nextButtonText: {
+        fontSize: 20,
+        fontFamily: FONT.OXYGEN_BOLD,
+        color: '#fff',
+    },
+    backButton: {
+        width: '35%',
         padding: 8,
         alignItems: 'center',
         justifyContent: 'center',
-        borderRadius: 8,
-        marginTop: 20,
+        borderRadius: 24,
+        backgroundColor: 'transparent',
     },
-    buttonText: {
+    backButtonText: {
+        fontSize: 18,
+        fontFamily: FONT.OXYGEN_BOLD,
+        color: '#333734',
+    },
+    startButtonContainer: {
+        width: '100%',
+        position: 'relative',
+        bottom: 0,
+        padding: 36,
+        alignItems: 'center',
+    },
+    startButton: {
+        backgroundColor: '#2E5E3D',
+        width: '45%',
+        padding: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 24,
+    },
+    startButtonText: {
         color: '#fff',
         fontWeight: 'bold',
         fontSize: 20,
