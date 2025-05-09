803d7e43df80fbf3deb436fe39bc2ea86aa125ac
marcellacarneiro <carneiromarcella53@gmail.com>
2025-05-08 21:37:43 -0300

    adiciona botão de mostrar senha

--- src/app/components/Input/index.tsx
+++ src/app/components/Input/index.tsx
@@ -6,13 +6,16 @@ import colors from '@/app/config/colors';
 
 interface InputProps extends TextInputProps {
     icon?: keyof typeof Ionicons.glyphMap;
+    showPasswordIcon?: keyof typeof Ionicons.glyphMap;
+    onShowPasswordIconPress?: () => void;
 }
 
-const Input = ({ icon, ...props }: InputProps) => {
+const Input = ({ icon, showPasswordIcon, onShowPasswordIconPress, ...props }: InputProps) => {
     return (
         <View style={styles.container}>
             {icon && <Ionicons name={icon} size={24} color={colors.green[800]} style={styles.icon} />}
             <TextInput {...props} style={styles.input} />
+            {showPasswordIcon && <Ionicons name={showPasswordIcon} size={24} color={colors.green[800]} onPress={onShowPasswordIconPress} />}
         </View>
     );
 };
